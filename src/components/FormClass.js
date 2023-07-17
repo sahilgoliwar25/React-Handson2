6fd8d88aa39051d10d400930ac05596bf82ca048
sahilgoliwar25 <sahilgoliwar1@gmail.com>
2023-07-17 15:28:19 +0530

    New Commit

---
+++ src/components/FormClass.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import './FormClass.css'
+
+export default class FormClass extends Component {
+    state={
+        Name: '',
+        Dept: '',
+        Rating:'',
+        EmpData: []
+    }
+    changeHandle=(event)=>{
+        this.setState({[event.target.name] : event.target.value})
+        // console.log(this.state.Name);
+        // console.log(this.state.Dept);
+        // console.log(this.state.Rating);
+    }
+    clickHandle=()=>{
+        let newObj={
+            name:this.state.Name,
+            dept:this.state.Dept,
+            rating:this.state.Rating
+        }
+        this.state.EmpData.push(newObj);
+        this.setState({EmpData : this.state.EmpData})
+        console.log(this.state.EmpData)
+        this.state.Name = '';
+        this.state.Dept = '';
+        this.state.Rating = '';
+    }
+  render() {
+    return (
+        <div className='body-container'>
+            <h1>EMPLOYEE FEEDBACK FORM</h1>
+            <form>
+                <>
+                <label htmlFor='name'>Name: </label>
+                <input id='name' type='text' placeholder='Enter Name' name='Name' value={this.state.Name} onChange={this.changeHandle} required/><br/>
+                </>
+                <>
+                <label htmlFor='dept'>Department: </label>
+                <input id='dept' type='text' placeholder='Enter Department' name='Dept' value={this.state.Dept} onChange={this.changeHandle} required/><br/>
+                </>
+                <>
+                <label htmlFor='rating'>Rating: </label>
+                <input id='rating' type='text' placeholder='Enter Rating' name='Rating' value={this.state.Rating} onChange={this.changeHandle} required/><br/>
+                </>
+                <button className='css-button-neumorphic' onClick={this.clickHandle} type='button'>Submit</button>
+            </form>
+            <div className='display-container'>
+            {this.state.EmpData.map((item,index) =>{
+                return(
+                    <div className='elements-container'>
+                        <div>Name: {item.name} | Department: {item.dept} | Rating: {item.rating}</div>
+                        
+                    </div>
+                )
+            })}
+            </div>
+      </div>
+    )
+  }
+}
